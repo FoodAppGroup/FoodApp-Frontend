7152d97bb5afac50f10515d9d2310e583b126dc9
Felix <steinke.felix@yahoo.de>
2020-01-20 23:57:39 +0100

    init startpage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,28 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PantryComponent} from './pantry/pantry.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {FoodTrackerComponent} from './food-tracker/food-tracker.component';
+import {AppComponent} from './app.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {path: 'pantry', component: PantryComponent},
+      {path: 'shopping-list', component: ShoppingListComponent},
+      {path: 'recipes', component: RecipesComponent},
+      {path: 'food-tracker', component: FoodTrackerComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {PantryComponent} from './pantry/pantry.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { FoodTrackerComponent } from './food-tracker/food-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PantryComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    FoodTrackerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
