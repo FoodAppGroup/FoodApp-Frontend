1c431ad41a969ce3b553939f30dbe608d25dfbcb
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-03-06 13:27:31 +0100

    fix typo

--- src/app/interfaces/food.ts
+++ src/app/interfaces/food.ts
@@ -1,9 +1,10 @@
 export interface Food {
   name: string;
   category: string; /* from the shop */
-  packageGramm: number; /* gramm in one package in the shop */
+  packageGram: number; /* gram in one package in the shop */
   kCal: number; /* per 100g */
   carbohydrates: number; /* per 100g */
   protein: number; /* per 100g */
   fat: number; /* per 100g */
+  unit: string; /* enum with gram or pieces */
 }
