9193b94e5aa47c3f4cbc737a5cb29087c0ddb584
Felix Steinke <82stfe1bif@hft-stuttgart.de>
2021-03-14 11:12:13 +0100

    update style from product pages

---
+++ src/app/interfaces/mock-products.ts
@@ -0,0 +1,45 @@
+import {Product} from './product';
+
+/* use hero.ts interface */
+export const PRODUCTS: Product[] = [
+  {
+    name: 'Penne',
+    category: 'Pasta',
+    packageGram: 500,
+    kCal: 100,
+    carbohydrates: 10,
+    protein: 10,
+    fat: 10,
+    unit: 'Gram'
+  },
+  {
+    name: 'Spaghetti',
+    category: 'Pasta',
+    packageGram: 500,
+    kCal: 100,
+    carbohydrates: 10,
+    protein: 10,
+    fat: 10,
+    unit: 'Gram'
+  },
+  {
+    name: 'Apfel',
+    category: 'Fruit',
+    packageGram: 500,
+    kCal: 100,
+    carbohydrates: 10,
+    protein: 10,
+    fat: 10,
+    unit: 'Unit'
+  },
+  {
+    name: 'Gurke',
+    category: 'Vegetable',
+    packageGram: 500,
+    kCal: 100,
+    carbohydrates: 10,
+    protein: 10,
+    fat: 10,
+    unit: 'Unit'
+  }
+];
